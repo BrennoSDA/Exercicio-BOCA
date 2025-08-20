a7277045c5e81183fca8ef651180c8e7705d6b0e
Brenno Soares <brennoaguiar@discente.ufg.br>
2025-08-19 21:25:42 -0300

    Update ex87.c

--- ex87.c
+++ ex87.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+
+    float a, r, areaCirculo, areaLateral, total, custo = 100.0, pi = 3.14159;
+
+    scanf("%f", &r);
+    scanf("%f", &a);
+
+    areaCirculo = (r * r) * pi;
+    areaLateral = (2 * pi * r) * a;
+
+    total = (2 * areaCirculo + areaLateral) * custo;
+
+    printf("O VALOR DO CUSTO E = %.2f\n", total);
+
+    return 0;
+}
